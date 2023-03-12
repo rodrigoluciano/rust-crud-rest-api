3292fea8ca5866c7b83177c7faf2abf3525b0a68
rodrigo <rodrigolucianocosta@gmail.com>
2023-03-12 06:19:01 -0300

    update contraints

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,24 +4,34 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        println!("Please input  your Guess.");
+        println!("Remember type a number!");
 
-    println!("Please input  your Guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        
+        let guess:u32 =  match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You gessed: {guess}");
 
-    println!("You gessed: {guess}");
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater =>println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater =>println!("Too big"),
-        Ordering::Equal => println!("You win"),
     }
 }
